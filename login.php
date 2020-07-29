d005989e332f7d4399b0ae6c17d828fdd59338d7
SHIVAM SINGH <57682629+shivamsingh3238@users.noreply.github.com>
2020-07-29 16:12:51 +0530

    Fix error

--- login.php
+++ login.php
@@ -20,7 +20,7 @@
     border-radius: 23px;
     /* border-style: inset; */
     background-color: white;
-	background-image: url(login.png);
+	background-image: url(Image/login.png);
 	background-size: 101% 113px;
     background-repeat: no-repeat;
     box-shadow: 0 0 15px 0 #494b4e;
