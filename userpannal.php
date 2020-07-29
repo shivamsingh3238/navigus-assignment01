eca6c45ae32499a0afe90724e8c572cad497020b
SHIVAM SINGH <57682629+shivamsingh3238@users.noreply.github.com>
2020-07-30 00:42:37 +0530

    Fix error

--- userpannal.php
+++ userpannal.php
@@ -149,6 +149,11 @@ p#py {
   <?php
             session_start();
             include 'connection.php';
+	    if(!isset($_SESSION['user_name']))
+	    {
+              header('location:index.php');
+              exit();
+            }
             $username = $_SESSION['user_name']; 
             
             $currtime = $_SESSION['Time'];
